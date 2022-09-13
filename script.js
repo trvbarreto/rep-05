046122ff20a9a635743551d8bab9d764b95febf2
Thales Barreto <trvbarreto@gmail.com>
2022-09-13 16:02:36 -0300

    Add subtract function and some tests

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function add(num1, num2) {
     return num1 + num2;
-}
+};
+
+function subtract(num1, num2) {
+    return (num1 < num2 ? num2 - num1 : num1 - num2);
+};
 
 // Tests
 console.log(add(1, 2));
-console.log(add(-1, -2));
+console.log(add(-1, -3));
 console.log(add("3", 3));
 console.log(add("teste", 4));
+
+console.log(subtract(1, 2));
+console.log(subtract(-1, -4));
+console.log(subtract("teste", 4));
