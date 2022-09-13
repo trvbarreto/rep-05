c8d81234d2c3b1022ba7a03e3b56ab23e0479d78
Thales Barreto <trvbarreto@gmail.com>
2022-09-13 16:03:45 -0300

    Add multiply function and some tests

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function subtract(num1, num2) {
     return (num1 < num2 ? num2 - num1 : num1 - num2);
 };
 
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
 // Tests
 console.log(add(1, 2));
 console.log(add(-1, -3));
@@ -15,3 +19,7 @@ console.log(add("teste", 4));
 console.log(subtract(1, 2));
 console.log(subtract(-1, -4));
 console.log(subtract("teste", 4));
+
+console.log(multiply(3, 2));
+console.log(multiply(-1, -4));
+console.log(multiply("teste", 4));
