d6d0f6d745163de089ac9e46b38c4d70f082c7bd
Thales Barreto <trvbarreto@gmail.com>
2022-09-13 16:04:37 -0300

    Add divide function and some tests

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ function multiply(num1, num2) {
     return num1 * num2;
 }
 
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
 // Tests
 console.log(add(1, 2));
 console.log(add(-1, -3));
@@ -23,3 +27,7 @@ console.log(subtract("teste", 4));
 console.log(multiply(3, 2));
 console.log(multiply(-1, -4));
 console.log(multiply("teste", 4));
+
+console.log(divide(3, 2));
+console.log(divide(-1, -4));
+console.log(divide("teste", 4));
